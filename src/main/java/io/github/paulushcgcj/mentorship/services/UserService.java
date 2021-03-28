97ac771edac6124a6d41ee3a0a2e887d0a131943
Paulo Gomes da Cruz Jr <paulushc@gmail.com>
2021-03-28 13:04:55 -0700

    feature: Implementing rest of UserService

--- src/main/java/io/github/paulushcgcj/mentorship/services/UserService.java
+++ src/main/java/io/github/paulushcgcj/mentorship/services/UserService.java
@@ -1,12 +1,20 @@
 package io.github.paulushcgcj.mentorship.services;
 
+import io.github.paulushcgcj.mentorship.exceptions.EntryNotFoundException;
 import io.github.paulushcgcj.mentorship.models.user.ServiceUser;
 import io.github.paulushcgcj.mentorship.repositories.GenericFileRepository;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.beanutils.BeanComparator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
 
 import java.nio.file.Path;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Stream;
 
 @Service
 @Slf4j
@@ -22,4 +30,43 @@ public class UserService {
     this.repository.setUp(stubFile, ServiceUser.class);
   }
 
+  public Mono<List<ServiceUser>> listCompanies(final int page, final int size, final String... sort) {
+    return repository.listEntries(page, size, userSorter(sort));
+  }
+
+  public Mono<ServiceUser> getUser(String userId) {
+    return repository.findById(userId);
+  }
+
+  public Mono<ServiceUser> updateUser(ServiceUser company) {
+    return
+      repository
+        .findById(company.getId())
+        .switchIfEmpty(Mono.error(new EntryNotFoundException(company.getId())))
+        .flatMap(existingCompany -> repository.save(company));
+  }
+
+  public Mono<ServiceUser> addUser(ServiceUser company) {
+    return repository.save(company);
+  }
+
+  public Mono<Void> removeUser(String userId) throws EntryNotFoundException {
+    return repository.remove(userId);
+  }
+
+  @SneakyThrows
+  private Comparator<ServiceUser> userSorter(final String... sort) {
+    if (sort != null) {
+      return
+        Stream
+          .of(sort)
+          .filter(paramName -> ServiceUser.comparingFields().contains(paramName))
+          .map((Function<String, BeanComparator<ServiceUser>>) BeanComparator::new)
+          .map(beanComparator -> (Comparator<ServiceUser>) beanComparator)
+          .reduce((c1, c2) -> c1.reversed().thenComparing(c2.reversed()))
+          .orElse(Comparator.comparing(ServiceUser::getId));
+    }
+
+    return Comparator.comparing(ServiceUser::getId).reversed();
+  }
 }
